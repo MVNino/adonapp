a20760c289938bf0c435d995f83a68f868b4ac40
Marlon Nino <mvnino.halcyondigital@gmail.com>
2021-03-10 18:02:12 +0800

    Uploading adonapp to git
    Signed-off-by: Marlon Nino <mvnino.halcyondigital@gmail.com>

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class PostController {
+    async index({ view }) {
+        const posts = [
+            {
+                title: 'Post One',
+                body: 'Post One body',
+            },
+            {
+                title: 'Post Two',
+                body: 'Post Two body',
+            },
+            {
+                title: 'Post Three',
+                body: 'Post Three body',
+            },
+        ]
+        
+        return view.render('posts.index', {
+            title: 'Marlon title',
+            posts: posts,
+        })
+    }
+
+
+}
+
+module.exports = PostController
